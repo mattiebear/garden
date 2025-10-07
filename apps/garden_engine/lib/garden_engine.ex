999837a04341108a82bc9663960c7a1e0b7e67da
Matthew Sells <matthew.sells@proton.me>
2025-10-06 20:25:58 -0500

    Remove unused module

--- apps/garden_engine/lib/garden_engine.ex
+++ apps/garden_engine/lib/garden_engine.ex
@@ -1,18 +1,2 @@
 defmodule GardenEngine do
-  @moduledoc """
-  Documentation for `GardenEngine`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GardenEngine.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/garden_engine/test/garden_engine_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GardenEngineTest do
-  use ExUnit.Case
-  doctest GardenEngine
-
-  test "greets the world" do
-    assert GardenEngine.hello() == :world
-  end
-end
