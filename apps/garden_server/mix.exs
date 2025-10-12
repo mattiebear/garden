7f4437f513fb6fdc7666205e861d0953dfafb5c7
Matthew Sells <matthew.sells@proton.me>
2025-10-12 13:28:42 -0500

    Add compose config

--- apps/garden_server/mix.exs
+++ apps/garden_server/mix.exs
@@ -69,7 +69,8 @@ defmodule GardenServer.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:garden_engine, in_umbrella: true}
     ]
   end
 
